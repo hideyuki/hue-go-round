d49b7dd07cf8f06d04aa8feef299309bd6a051e3
Hideyuki Takei <takehide22@gmail.com>
2015-03-28 02:48:20 +0900

    go around.

---
+++ README.md
@@ -0,0 +1,13 @@
+# hue-go-round
+
+For Beatrobo office only.
+
+---
+
+## How to run
+
+```
+$ export HUE_USER_NAME=your-user-name
+$ node index.js
+```
+
--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 var hue = require("node-hue-api");
-var HueApi = require("node-hue-api").HueApi;
+var HueApi = hue.HueApi;
+var lightState = hue.lightState;
 
-// Global
+// Dirty global
 var bridgeIp;
 var userName = process.env.HUE_USER_NAME || "";
 var hueApi;
+var lightIds = [3, 8, 5, 7, 13, 12, 6, 1, 9, 11, 10, 2, 4];
+var index = 0;
 
-if(userName){
-  hue.nupnpSearch().then(function(bridge){
-    if(bridge.length>0){
-      bridgeIp = bridge[0].ipaddress;
-      getLightInfo();
+// Search bridge
+hue.nupnpSearch().then(function(bridges){
+  if(bridges.length>0){
+    bridgeIp = bridges[0].ipaddress;
+    console.log("Bridge IP: " + bridgeIp);
+
+    // create hueApi object
+    hueApi = new HueApi(bridgeIp, userName);
+
+    goAround();
+  }
+}).done();
+
+// hue go around 
+function goAround(){
+  setInterval(function(){
+    setLight(lightIds[index], true);
+    setLight(getPreviousLightIdByIndex(index), false);
+    index++;
+    if(index>=lightIds.length){
+      index = 0;
     }
-  }).done();
-} else {
-  console.log("Please add the env variable HUE_USER_NAME");
+  }, 200);
 }
 
-var lightState = hue.lightState;
+function getPreviousLightIdByIndex(index){
+  if(index==0){
+    return lightIds[lightIds.length - 1];
+  } else {
+    return lightIds[index - 1];
+  }
+}
 
-function getLightInfo(){
-  hueApi = new HueApi(bridgeIp, userName);
-
-  hueApi.lights().then(function(result){
-    var lights = result.lights;
-    var state = lightState.create().on().white(500, 0);
-
-    for(var i=0; i<lights.length; i++){
-      var id = lights[i].id;
-      hueApi.setLightState(id, state);
-    }
-  }).done();
+function setLight(id, sw){
+  if(sw){
+    console.log(id + ": on");
+    hueApi.setLightState(id, lightState.create().rgb(255, 255, 255).on());
+  } else {
+    console.log(id + ": off");
+    hueApi.setLightState(id, lightState.create().off());
+  }
 }
 
